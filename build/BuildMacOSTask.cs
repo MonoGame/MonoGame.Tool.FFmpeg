73e9a3e13819268c455710b880f56170a17983f3
Christopher Whitley <aristurtledev@gmail.com>
2024-04-28 22:18:06 -0400

    Simplifying

--- build/BuildMacOSTask.cs
+++ build/BuildMacOSTask.cs
@@ -12,81 +12,156 @@ public sealed class BuildMacOSTask : BuildTaskBase
     public override void Run(BuildContext context)
     {
         // Determine which mac architecture(s) to build for.
-        var buildx8664 = context.IsUniversalBinary || RuntimeInformation.ProcessArchitecture is not Architecture.Arm or Architecture.Arm64;
+        var buildX64 = context.IsUniversalBinary || RuntimeInformation.ProcessArchitecture is not Architecture.Arm or Architecture.Arm64;
         var buildArm64 = context.IsUniversalBinary || RuntimeInformation.ProcessArchitecture is Architecture.Arm or Architecture.Arm64;
 
+        if (buildX64)
+            BuildX64(context);
+
+        if(buildArm64)
+            BuildArm64(context);
+
+        
+        
+
+
+        // // Generate common build directory paths for each architectures build artifacts
+        // var x866BuildDirectory = $"{absoluteArtifactDir}/osx-x86_64";
+        // var arm64BuildDirectory = $"{absoluteArtifactDir}/osx-arm64";
+
+        // if (buildX64)
+        // {
+        //     // Create the build settings used by each library build
+        //     var buildSettings = new BuildSettings()
+        //     {
+        //         ShellCommand = "zsh",
+        //         PrefixFlag = x866BuildDirectory,
+        //         PkgConfigPath = $"{x866BuildDirectory}/lib/pkgconfig",
+        //         HostFlag = "x86_64-apple-darwin",
+        //         CFlags = $"-w -arch x86_64 -I{x866BuildDirectory}/include",
+        //         CPPFlags = $"-arch x86_64 -I{x866BuildDirectory}/include",
+        //         CXXFlags = "-arch x86_84",
+        //         LDFlags = $"-arch x86_64 -L{x866BuildDirectory}/lib"
+        //     };
+
+        //     // Get the configuration flags that will be used for the FFMpeg build
+        //     var x8664FFMpegConfigureFlags = GetFFMpegConfigureFlags(context, "osx-x86_64");
+
+        //     // Build each library in correct order
+        //     BuildOgg(context, buildSettings);
+        //     BuildVorbis(context, buildSettings);
+        //     BuildLame(context, buildSettings);
+        //     BuildFFMpeg(context, buildSettings, x8664FFMpegConfigureFlags);
+        // }
+
+        // if (buildArm64)
+        // {
+        //     // Create the build settings used by each library build
+        //     var buildSettings = new BuildSettings()
+        //     {
+        //         ShellCommand = "zsh",
+        //         PrefixFlag = arm64BuildDirectory,
+        //         PkgConfigPath = $"{arm64BuildDirectory}/lib/pkgconfig",
+        //         HostFlag = "aarch64-apple-darwin",
+        //         CFlags = $"-w -arch arm64 -I{arm64BuildDirectory}/include",
+        //         CPPFlags = $"-arch arm64 -I{arm64BuildDirectory}/include",
+        //         CXXFlags = "-arch arm64",
+        //         LDFlags = $"-arch arm64 -L{arm64BuildDirectory}/lib"
+        //     };
+
+        //     // Get the configuration flags that will be used for the FFMpeg build
+        //     var arm64FFMpegConfigureFlags = GetFFMpegConfigureFlags(context, "osx-arm64");
+
+        //     BuildOgg(context, buildSettings);
+        //     BuildVorbis(context, buildSettings);
+        //     BuildLame(context, buildSettings);
+        //     BuildFFMpeg(context, buildSettings, arm64FFMpegConfigureFlags);
+        // }
+
+        // // Move the build binary from the build directory to the artifact directory.
+        // // If this is a universal build, we'll need to combine both binaries using lipo and output the result of that.
+        // if (buildX64 && buildArm64)
+        // {
+        //     context.StartProcess("lipo", new ProcessSettings()
+        //     {
+        //         Arguments = $"-create {x866BuildDirectory}/bin/ffmpeg {arm64BuildDirectory}/bin/ffmpeg -output {absoluteArtifactDir}/ffmpeg"
+        //     });
+        // }
+        // else if (buildX64)
+        // {
+        //     context.CopyFile($"{x866BuildDirectory}/bin/ffmpeg", $"{absoluteArtifactDir}/ffmpeg");
+        // }
+        // else
+        // {
+        //     context.CopyFile($"{arm64BuildDirectory}/bin/ffmpeg", $"{absoluteArtifactDir}/ffmpeg");
+        // }
+    }
+
+    private static void BuildX64(BuildContext context)
+    {
         // Absolute path to the artifact directory is needed for flags since they don't allow relative path
         var absoluteArtifactDir = context.MakeAbsolute(new DirectoryPath(context.ArtifactsDir));
+        var cFlagsExport = "export CFLAGS=\"-w -arch x86_64\";";
+        var ccFlagsExport = "export CCFLAGS=\"-arch x86_64\";";
+        var ldFlagsExport = "export LDFLAGS=\"-arch x86_64\";";
+        var pkgConfigExport = "";
+        var exports = $"{cFlagsExport}{ccFlagsExport}{ldFlagsExport}{pkgConfigExport}";
 
-        // Generate common build directory paths for each architectures build artifacts
-        var x866BuildDirectory = $"{absoluteArtifactDir}/osx-x86_64";
-        var arm64BuildDirectory = $"{absoluteArtifactDir}/osx-arm64";
+        var configureFlags = GetFFMpegConfigureFlags(context, "windows-x64");
+        var processSettings = new ProcessSettings() { WorkingDirectory = "./ffmpeg" };
+        var shellCommandPath = "zsh";
 
-        if (buildx8664)
+        // Ensure clean start if we're running locally and testing over and over
+        if (context.BuildSystem().IsLocalBuild)
         {
-            // Create the build settings used by each library build
-            var buildSettings = new BuildSettings()
-            {
-                ShellCommand = "zsh",
-                PrefixFlag = x866BuildDirectory,
-                PkgConfigPath = $"{x866BuildDirectory}/lib/pkgconfig",
-                HostFlag = "x86_64-apple-darwin",
-                CFlags = $"-w -arch x86_64 -I{x866BuildDirectory}/include",
-                CPPFlags = $"-arch x86_64 -I{x866BuildDirectory}/include",
-                CXXFlags = "-arch x86_84",
-                LDFlags = $"-arch x86_64 -L{x866BuildDirectory}/lib"
-            };
-
-            // Get the configuration flags that will be used for the FFMpeg build
-            var x8664FFMpegConfigureFlags = GetFFMpegConfigureFlags(context, "osx-x86_64");
-
-            // Build each library in correct order
-            BuildOgg(context, buildSettings);
-            BuildVorbis(context, buildSettings);
-            BuildLame(context, buildSettings);
-            BuildFFMpeg(context, buildSettings, x8664FFMpegConfigureFlags);
+            processSettings.Arguments = $"-c \"{exports} make distclean\"";
+            context.StartProcess(shellCommandPath, processSettings);
         }
 
-        if (buildArm64)
-        {
-            // Create the build settings used by each library build
-            var buildSettings = new BuildSettings()
-            {
-                ShellCommand = "zsh",
-                PrefixFlag = arm64BuildDirectory,
-                PkgConfigPath = $"{arm64BuildDirectory}/lib/pkgconfig",
-                HostFlag = "aarch64-apple-darwin",
-                CFlags = $"-w -arch arm64 -I{arm64BuildDirectory}/include",
-                CPPFlags = $"-arch arm64 -I{arm64BuildDirectory}/include",
-                CXXFlags = "-arch arm64",
-                LDFlags = $"-arch arm64 -L{arm64BuildDirectory}/lib"
-            };
+        // Run configure to build make file
+        processSettings.Arguments = $"-c \"{exports} ./configure --bindir={absoluteArtifactDir} {configureFlags}\"";
+        context.StartProcess(shellCommandPath, processSettings);
 
-            // Get the configuration flags that will be used for the FFMpeg build
-            var arm64FFMpegConfigureFlags = GetFFMpegConfigureFlags(context, "osx-arm64");
+        // Run make
+        processSettings.Arguments = $"-c \"{exports} make -j{Environment.ProcessorCount}\"";
+        context.StartProcess(shellCommandPath, processSettings);
 
-            BuildOgg(context, buildSettings);
-            BuildVorbis(context, buildSettings);
-            BuildLame(context, buildSettings);
-            BuildFFMpeg(context, buildSettings, arm64FFMpegConfigureFlags);
+        // Run make install
+        processSettings.Arguments = $"-c \"{exports} make install\"";
+        context.StartProcess(shellCommandPath, processSettings);
+    }
+
+    private static void BuildArm64(BuildContext context)
+    {
+        // Absolute path to the artifact directory is needed for flags since they don't allow relative path
+        var absoluteArtifactDir = context.MakeAbsolute(new DirectoryPath(context.ArtifactsDir));
+        var cFlagsExport = "export CFLAGS=\"-w -arch arm64\";";
+        var ccFlagsExport = "export CCFLAGS=\"-arch arm64\";";
+        var ldFlagsExport = "export LDFLAGS=\"-arch arm64\";";
+        var pkgConfigExport = "";
+        var exports = $"{cFlagsExport}{ccFlagsExport}{ldFlagsExport}{pkgConfigExport}";
+
+        var configureFlags = GetFFMpegConfigureFlags(context, "windows-x64");
+        var processSettings = new ProcessSettings() { WorkingDirectory = "./ffmpeg" };
+        var shellCommandPath = "zsh";
+
+        // Ensure clean start if we're running locally and testing over and over
+        if (context.BuildSystem().IsLocalBuild)
+        {
+            processSettings.Arguments = $"-c \"{exports} make distclean\"";
+            context.StartProcess(shellCommandPath, processSettings);
         }
 
-        // Move the build binary from the build directory to the artifact directory.
-        // If this is a universal build, we'll need to combine both binaries using lipo and output the result of that.
-        if (buildx8664 && buildArm64)
-        {
-            context.StartProcess("lipo", new ProcessSettings()
-            {
-                Arguments = $"-create {x866BuildDirectory}/bin/ffmpeg {arm64BuildDirectory}/bin/ffmpeg -output {absoluteArtifactDir}/ffmpeg"
-            });
-        }
-        else if (buildx8664)
-        {
-            context.CopyFile($"{x866BuildDirectory}/bin/ffmpeg", $"{absoluteArtifactDir}/ffmpeg");
-        }
-        else
-        {
-            context.CopyFile($"{arm64BuildDirectory}/bin/ffmpeg", $"{absoluteArtifactDir}/ffmpeg");
-        }
+        // Run configure to build make file
+        processSettings.Arguments = $"-c \"{exports} ./configure --bindir={absoluteArtifactDir} {configureFlags}\"";
+        context.StartProcess(shellCommandPath, processSettings);
+
+        // Run make
+        processSettings.Arguments = $"-c \"{exports} make -j{Environment.ProcessorCount}\"";
+        context.StartProcess(shellCommandPath, processSettings);
+
+        // Run make install
+        processSettings.Arguments = $"-c \"{exports} make install\"";
+        context.StartProcess(shellCommandPath, processSettings);
     }
 }
--- build/BuildWIndowsTask.cs
+++ build/BuildWIndowsTask.cs
@@ -11,42 +11,34 @@ public sealed class BuildWindowsTask : BuildTaskBase
     {
         // Absolute path to the artifact directory is needed for flags since they don't allow relative path
         var absoluteArtifactDir = context.MakeAbsolute(new DirectoryPath(context.ArtifactsDir)).ToString();
+        var cFlagsExport = "export CFLAGS=\"-w\";";
+        var ccFlagsExport = "export CCFLAGS=\"x86_64-w64-mingw32-gcc\";";
+        var ldFlagsExport = "export LDFLAGS=\"--static\";";
+        var pathExport = "export PATH=\"/usr/bin:/mingw64/bin:$PATH\";";
+        var pkgConfigExport = "export PKG_CONFIG_PATH=\"/mingw64/lib/pkgconfig:$PKG_CONFIG_PATH\";";
+        var exports = $"{pathExport}{cFlagsExport}{ccFlagsExport}{ldFlagsExport}{pkgConfigExport}";
 
-        // Since we are using mingw to build, paths need to be in unix format
-        // e.g. C:\Users\MonoGame\Desktop\ => /c/Users/MonoGame/Desktop
-        absoluteArtifactDir = absoluteArtifactDir.Replace("\\", "/");
-        absoluteArtifactDir = $"/{absoluteArtifactDir[0]}{absoluteArtifactDir[2..]}";
+        var configureFlags = GetFFMpegConfigureFlags(context, "windows-x64");
+        var processSettings = new ProcessSettings() { WorkingDirectory = "./ffmpeg" };
+        var shellCommandPath = @"C:\msys64\usr\bin\bash.exe";
 
-        // Generate common build directory path
-        var buildDirectory = $"{absoluteArtifactDir}/windows-x86_64";
-        context.CreateDirectory(buildDirectory);
-
-        // Create the build settings used by each library build
-        var buildSettings = new BuildSettings
+        // Ensure clean start if we're running locally and testing over and over
+        if (context.BuildSystem().IsLocalBuild)
         {
-            ShellCommand = @"C:\msys64\usr\bin\bash",
-            PrefixFlag = buildDirectory,
-            HostFlag = "x86_64-w64-mingw32",
-            PkgConfigPath = $"{buildDirectory}/lib/pkgconfig",
-            Path = "/usr/bin:/mingw64/bin",
-            CCFlags = "x86_64-w64-mingw32-gcc",
-            CFlags = $"-w -I{buildDirectory}/include",
-            CPPFlags = $"-I{buildDirectory}/include",
-            LDFlags = $"-L{buildDirectory}/lib --static"
-        };
+            processSettings.Arguments = $"-c \"{exports} make distclean\"";
+            context.StartProcess(shellCommandPath, processSettings);
+        }
 
-        // Get the configuration flags that will be used for the FFMpeg build
-        var ffmpegConfigureFlags = GetFFMpegConfigureFlags(context, "windows-x86_64");
+        // Run configure to build make file
+        processSettings.Arguments = $"-c \"{exports} ./configure --bindir={absoluteArtifactDir} {configureFlags}\"";
+        context.StartProcess(shellCommandPath, processSettings);
 
-        // Build each library in correct order
-        BuildOgg(context, buildSettings);
-        BuildVorbis(context, buildSettings);
-        BuildLame(context, buildSettings);
-        BuildFFMpeg(context, buildSettings, ffmpegConfigureFlags);
+        // Run make
+        processSettings.Arguments = $"-c \"{exports} make -j{Environment.ProcessorCount}\"";
+        context.StartProcess(shellCommandPath, processSettings);
 
-        // Move the built binary from the build directory to the artifact directory
-        // Note: For some reason, unlike the linux and mac builds, the windows build will not copy the binary using the
-        // unix like path created. It fails to find it. Instead, a relative path is used here for windows.
-        context.CopyFile($"{context.ArtifactsDir}/windows-x86_64/bin/ffmpeg.exe", $"{context.ArtifactsDir}/ffmpeg.exe");
+        // Run make install
+        processSettings.Arguments = $"-c \"{exports} make install\"";
+        context.StartProcess(shellCommandPath, processSettings);
     }
 }
